c89fd4a2602066689b19619b1cc3d8ebb04569f7
VladislavMashkov <Vlad_mashkow@mail.ru>
2017-11-19 19:03:08 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,10 +1,10 @@
 #include <catch.hpp>
-#include <main.hpp>
+#include "main.hpp"
 TEST_CASE("Check functions", "[explorer]") {
 	SECTION("Check INIT")
 	{
 		fs::path Path = "dir";
-		bool ok = print_info(Path);
+		bool ok = print_data(Path);
 		REQUIRE( ok == true);
 	}
 }
