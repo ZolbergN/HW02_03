fe76fff47139eb64a52a2f39e21bb2065afad020
VladislavMashkov <Vlad_mashkow@mail.ru>
2017-11-19 19:17:40 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -3,6 +3,7 @@
 TEST_CASE("Check functions", "[explorer]") {
 	SECTION("Check INIT")
 	{
+		fs::create_directory("dir");
 		fs::path Path("dir");
 		bool ok = print_data(Path);
 		cout << ok << endl;
